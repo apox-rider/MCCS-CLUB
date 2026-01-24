0e91370f62c37ac6eab37387ad9543144fd3a182
apox-rider <apolinaryavith@gmail.com>
2026-01-24 12:25:24 +0300

    accomplish

--- mobileMoneyTransaction Tracker/transaction.py
+++ mobileMoneyTransaction Tracker/transaction.py
@@ -11,28 +11,37 @@ try:
         incomeAmount.append(income)
         count-=1
         totalIncome=sum(incomeAmount)
+    
+    
     numberOfExpenditure=int(input("Input number of expense transactions:  "))
-    nom=numberOfExpenditure
-    try:
-        while nom>0:
+    nom=numberOfExpenditure    
+    while nom>0:
             expense=int(input( "Expense ammount:  "))
             expenseAmount.append(expense)
             nom-=1
             totalExpenses=sum(expenseAmount)
-        balance=totalIncome-totalExpenses
-        transactionList=expenseAmount.append(incomeAmount)
-        totalTransaction=len(incomeAmount)+len(expenseAmount)
-        print(f"Total number of transactions is {len(transactionList)}")
-        print(f"The highest transaction is {max(max(incomeAmount) and max(expenseAmount))}")
-        print(f"The lowest transaction is {min(min(incomeAmount) and min(expenseAmount))}")
-        if balance<0:
+    print(totalIncome)
+    print(expenseAmount)
+    am3=max(expenseAmount)
+    am5=min(expenseAmount)
+    print(totalExpenses)
+    balance=totalIncome-totalExpenses
+    transactionList=expenseAmount.append(incomeAmount)
+    totalTransaction=len(incomeAmount)+len(expenseAmount)
+    am1=len(incomeAmount)
+    am2=len(expenseAmount)-1
+    am4=max(incomeAmount)
+    am6=min(incomeAmount)
+    print(f"Total number of transactions is { am1+am2}")
+    print(f"The highest transaction is {max(am3,am4)}")
+    print(f"The lowest transaction is {min( am5,am6)}")
+    if balance<0:
             print("Warning!!!!!! Total expenses are greater than balance, beaware of your expenditure")
-        elif balance>0:
+    elif balance>0:
             print(f"Account balance is {totalIncome-totalExpenses} ")
-        elif balance==0:
+    elif balance==0:
             print("Your account balance is zero(0)")
-    except ValueError:
-        print("Input Error  ")
+     
 except ValueError:
     print("Input Error during Entry")
 
