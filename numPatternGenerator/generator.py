91672aff0c42392a54de8880fb78546c69a543e6
apox-rider <apolinaryavith@gmail.com>
2026-01-29 20:15:38 +0300

    all1

--- numPatternGenerator/generator.py
+++ numPatternGenerator/generator.py
@@ -1,14 +1,14 @@
-print ("Welcome to the number pattern generator")
-choice=input("Want to preceed?(yes/no)")
+print ("Welcome to the number pattern generator ")
+choice=input("Want to preceed?(yes/no) ")
 if choice=="yes":
     while choice:
         try:
-            n = int(input("Enter a number: "))  
+            n = int(input("Enter a number:  "))  
             for row in range(1, n+1):            
                 for number in range(1, row+1):   
                     print(number, end=" ")        
                 print()                        
         except ValueError:
-            print("Input not number")
+            print("Input not number ")
 else:
-    print("Pattern generator closed" )
+    print("Pattern generator closed " )
