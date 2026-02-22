8355088980ccce14adc96b048f69789131df947d
apox-rider <apolinaryavith@gmail.com>
2026-02-22 22:19:44 +0300

    22022026

--- supermarket/bill.py
+++ supermarket/bill.py
@@ -8,7 +8,7 @@ items=[]
 def record():
     time.sleep(3)
     print("Initializing system...")
-    time.sleep(4)
+    time.sleep(4) 
     print("ACTIVITIES")
     activity=input("A:Input product details \nB:Exit \nOption : ")
     if activity.strip().capitalize()=="A":
